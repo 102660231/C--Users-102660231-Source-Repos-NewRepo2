5a11213856cc9d35e1d6f64edd4911210b3cf2af
102660231 <48194706+102660231@users.noreply.github.com>
2019-05-06 17:00:14 +1000

    add

--- Program.cs
+++ Program.cs
@@ -12,43 +12,46 @@ namespace ConsoleApp1
             int rollnum = 0;
             int sum = 0;
             int averagesum = 0;
-            int rollthrow = 0;
+          
 
-            // set initial length for array a
-            int x = 0;
             string Answer = "Y";
 
             bool endroll = false;
             int counter = 0;
-            //int rollcounter = 0;
-
+            int rollnum3 = 0;
+            int rollnum4 = 0;
        
             while (endroll==false)
             {
                 Random dies = new Random();
                 int Number = dies.Next(1, 7);
                 rollnum = Number;
+                Random dies3 = new Random();
+                int Number3 = dies3.Next(1, 7);
+                rollnum3 = Number3;
 
-                Console.WriteLine("  system rolled number is : " + Number + "");
+                Console.WriteLine("  system rolled number is : " + Number + "  .  "+Number3);
                 Console.WriteLine("  DO you want to roll your number  Y/N? ");
                 Answer = Console.ReadLine();
                 Answer = Answer.ToUpper();
+
                 if (Answer == "Y")
                 {
 
                     Random dies1 = new Random();
                     int Number1 = dies1.Next(1, 7);
-                    Console.WriteLine("          your rolled number is :" + Number1 + " ");
+                    Random dies4 = new Random();
+                    int Number4 = dies4.Next(1, 7);
+
+                    Console.WriteLine("          your rolled number is :" + Number1 + " + "+Number4);
                    
                     Console.ReadKey();
                     rollnum = Number1;
-                    
+                    rollnum4 = Number4;
                 }
                 counter++;
 
-                
-
-                x++;
+                //x++;
 
                 Console.Write("DO you want to continue to continue to  ? Y/N   ");
                 Answer = Console.ReadLine();
@@ -58,29 +61,14 @@ namespace ConsoleApp1
                 {
 
                     sum = sum + rollnum;
-
-                    averagesum = (sum / counter);
+                   
+                    averagesum = sum / counter;
                     Console.WriteLine("sum : " + sum + "  " + "average: " + averagesum + " ");
                     Console.ReadKey();
-                    Console.WriteLine("End of roll !")
                     endroll = true;              
                 }
             }
-            int row1 = 0;
-            int col1 = 0;
-
-            int die1 = 0;
-            int die2 = 0;
-            int[,] sums = new int[row1, col1];
-            Random num = new Random();
-
-            
-
-           // for (int i = 0; i < x; i++) 
-           // {
-            //    Console.Write("{0},value[i]");
-              //  Console.ReadKey();
-           // };
+          
 
         }   
 
